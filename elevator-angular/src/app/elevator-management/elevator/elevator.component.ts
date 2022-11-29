3ad547c26b9653d8847e4397cf7313e77ca6d1cb
marcin <marcin7848@gmail.com>
2022-11-29 22:18:31 +0100

    passing elevators changes

--- elevator-angular/src/app/elevator-management/elevator/elevator.component.ts
+++ elevator-angular/src/app/elevator-management/elevator/elevator.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Elevator} from "../../model/Elevator";
 
 @Component({
   selector: 'app-elevator',
   templateUrl: './elevator.component.html',
   styleUrls: ['./elevator.component.css']
 })
-export class ElevatorComponent {
+export class ElevatorComponent implements OnInit {
+  @Input() elevator: Elevator = new Elevator();
 
+
+  ngOnInit() {
+    console.log("on ngOnInit");
+  }
+
+  ngOnChanges(){
+    console.log("on changes");
+  }
 }
--- elevator-angular/src/app/elevator-management/management/management.component.ts
+++ elevator-angular/src/app/elevator-management/management/management.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Elevator} from "../../model/Elevator";
 
 @Component({
   selector: 'app-management',
   templateUrl: './management.component.html',
   styleUrls: ['./management.component.css']
 })
-export class ManagementComponent {
+export class ManagementComponent implements OnInit {
+  elevator: Elevator;
+  constructor(){
+    this.elevator = new Elevator(5, 2, []);
+  }
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.elevator.id = 98;
+    }, 5000);
+  }
 }
