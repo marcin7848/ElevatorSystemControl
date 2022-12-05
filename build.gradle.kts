4a4805be5ef395288fa5ef93153ba566ba549ac1
marcin <marcin7848@gmail.com>
2022-12-06 00:54:48 +0100

    gradle updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,11 @@ plugins {
     java
 }
 
-tasks.register("createAppDirectory") {
-    doLast {
-        mkdir("createAppDirectory")
-    }
-}
-
 tasks.register<Copy>("copyJar") {
     from("elevator-backend/build/libs/elevator-backend-1.0.jar")
     into("app/")
 }
 
 tasks.build {
-    finalizedBy("createAppDirectory", "copyJar")
+    finalizedBy( "copyJar")
 }
